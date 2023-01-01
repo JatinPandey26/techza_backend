4fe468b231ab82bb26dd9fa0c8c31e08779de881
Jatin pandey <jatin26pandey@gmail.com>
2023-01-01 21:30:12 +0530

    cors

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+
 app.use("/api/v1", courseRoute);
 app.use("/api/v1", userRoute);
 app.use("/api/v1", paymentRoute);
