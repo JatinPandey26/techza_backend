329aec68eebf899d2a101a7f2e0ff99f83633064
Jatin pandey <jatin26pandey@gmail.com>
2023-01-01 21:13:29 +0530

    cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
